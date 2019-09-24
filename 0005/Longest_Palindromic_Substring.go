a896008c4f18055dd677ada182fca7862962104d
tdytaylor <949722633@qq.com>
2019-09-24 17:14:50 +0800

    0005 动态规划template

--- 0005/Longest_Palindromic_Substring.go
+++ 0005/Longest_Palindromic_Substring.go
@@ -4,18 +4,15 @@ import "fmt"
 
 func main() {
 	//fmt.Printf("%v\n", isHuiwen("aa"))
-	fmt.Println(longestPalindrome("aa"))
+	fmt.Println(longestPalindrome("a"))
 }
 
 // 暴力破解法
 func longestPalindrome(s string) string {
 	num, ans, l := 0, "", len(s)
-	if l == 0 || l == 1 {
-		return s
-	}
 	for i := 0; i < l; i++ {
 		for j := i + 1; j <= l; j++ {
-			if isHuiwen(s[i:j]) {
+			if isPalindromicString(s[i:j]) {
 				if (j - i) > num {
 					num = j - i
 					ans = s[i:j]
@@ -26,11 +23,27 @@ func longestPalindrome(s string) string {
 	return ans
 }
 
-func isHuiwen(s string) bool {
+func isPalindromicString(s string) bool {
 	for i, j := 0, len(s); i < j/2; i++ {
 		if s[i] != s[j-1-i] {
 			return false
 		}
 	}
 	return true
+}
+
+// 动态规划
+func longestPalindrome2(s string) string {
+	num, ans, l := 0, "", len(s)
+	for i := 0; i < l; i++ {
+		for j := i + 1; j <= l; j++ {
+			if isPalindromicString(s[i:j]) {
+				if (j - i) > num {
+					num = j - i
+					ans = s[i:j]
+				}
+			}
+		}
+	}
+	return ans
 }
