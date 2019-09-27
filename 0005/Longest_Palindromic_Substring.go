96e45d433d6004c80b771ff5a43f38cd66040568
tdytaylor <949722633@qq.com>
2019-09-27 16:34:10 +0800

    0005 动态规划实现

--- 0005/Longest_Palindromic_Substring.go
+++ 0005/Longest_Palindromic_Substring.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	//fmt.Printf("%v\n", isHuiwen("aa"))
-	fmt.Println(longestPalindrome("a"))
+	fmt.Println(longestPalindrome2("abb"))
 }
 
 // 暴力破解法
@@ -34,16 +34,32 @@ func isPalindromicString(s string) bool {
 
 // 动态规划
 func longestPalindrome2(s string) string {
-	num, ans, l := 0, "", len(s)
-	for i := 0; i < l; i++ {
-		for j := i + 1; j <= l; j++ {
-			if isPalindromicString(s[i:j]) {
-				if (j - i) > num {
-					num = j - i
-					ans = s[i:j]
+	len, maxLen, start := len(s), 0, 0
+
+	var dp = make([][]int, len)
+	for i := 0; i < len; i++ {
+		dp[i] = make([]int, len)
+		dp[i][i] = 1
+		if maxLen < 2 {
+			maxLen = 1
+		}
+		if i < len-1 && s[i] == s[i+1] {
+			dp[i][i+1] = 1
+			start = i
+			maxLen = 2
+		}
+	}
+
+	for i := 3; i <= len; i++ { // 每次检索的字符串长度，依次递增
+		for j := 0; j+i-1 < len; j++ {
+			if s[j] == s[j+i-1] && dp[j+1][j+i-2] == 1 {
+				dp[j][j+i-1] = 1
+				if i >= maxLen {
+					start = j
+					maxLen = i
 				}
 			}
 		}
 	}
-	return ans
+	return s[start : start+maxLen]
 }
