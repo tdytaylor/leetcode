44a7330535fd93edcc8c74afe1e99d70c61b9d7c
tdytaylor <tangdaiyao@zbj.com>
2020-08-28 17:46:46 +0800

    3Sum-add tests

--- issue_0015/three_sum_test.go
+++ issue_0015/three_sum_test.go
@@ -33,3 +33,32 @@ func Test_threeSum(t *testing.T) {
 		})
 	}
 }
+
+func Test_threeSum2(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				nums: []int{0, 0, 0},
+			},
+			want: [][]int{
+				{0, 0, 0},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum2(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
